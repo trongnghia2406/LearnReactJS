1a3a77bcfc1d296bc30ee4ff41d0a0a82cd422a6
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-11-22 17:21:44 +0700

    chapter 1 - ES6: lesson 4: arrow function

---
+++ Chapter1_JSES6/bai4_arrowfunction/bai4.js
@@ -0,0 +1,9 @@
+test = () => console.log("Arrow function test");
+hello = () => "Hello, Arrow Function!";
+const add = (a, b) => a + b;
+const multiply = (a, b) => {
+    return a * b;
+}
+console.log(test());
+console.log(hello());
+console.log("Addition:", add(5, 3));
