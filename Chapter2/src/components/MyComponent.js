16f2c2fa9316595558bcd262d865d4f75350e49f
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-12-10 22:11:27 +0700

    Chapter 2 - ReactBasic: lesson3: convert app to class component, State in react

--- Chapter2/src/components/MyComponent.js
+++ Chapter2/src/components/MyComponent.js
@@ -1,10 +1,15 @@
 import React from "react";
 class MyComponent extends React.Component {
+    state = {
+        name: 'BangThan',
+        address: 'Binh Thuan',
+        age: 21
+    };
     // JSX
     render() {
         return (
-            <div>my first component
-
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address};
             </div>
         );
     }
