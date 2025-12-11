730da9049da22d925b209386d9b1e242a001d03d
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-12-11 16:21:42 +0700

    Chapter 2 - ReactBasic: lesson4: React DOM Events

--- Chapter2/src/components/MyComponent.js
+++ Chapter2/src/components/MyComponent.js
@@ -5,11 +5,19 @@ class MyComponent extends React.Component {
         address: 'Binh Thuan',
         age: 21
     };
+    handleClick(event) {
+        console.log(event);
+    }
+    handleOnMoveOver(event) {
+        console.log(event.pageX);
+    }
     // JSX
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address};
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMoveOver}>Hover me</button>
             </div>
         );
     }
