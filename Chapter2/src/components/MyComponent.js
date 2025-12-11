e39d3ecdbbfa6669d12d34dda7e25f9eb6a20304
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-12-11 21:32:48 +0700

    Chapter 2 - ReactBasic: lesson7: nesting components

--- Chapter2/src/components/MyComponent.js
+++ Chapter2/src/components/MyComponent.js
@@ -1,44 +1,11 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 class MyComponent extends React.Component {
-    state = {
-        name: 'BangThan',
-        address: 'Binh Thuan',
-        age: 21
-    };
-    handleClick(event) {
-        console.log('click me');
-        this.setState(
-            {
-                name: 'LeTrongNghia',
-                age: Math.floor((Math.random() * 100) + 1)
-            }
-        )
-    }
-    handleOnMoveOver(event) {
-        // console.log(event.pageX);
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        });
-    }
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age};
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <input
-                        type="text"
-                        onChange={(event) => this.handleOnChangeInput(event)}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfo />
             </div>
         );
     }
---
+++ Chapter2/src/components/UserInfo.js
@@ -0,0 +1,53 @@
+import React from "react";
+class UserInfo extends React.Component {
+    state = {
+        name: 'BangThan',
+        address: 'Binh Thuan',
+        age: 21
+    };
+
+
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    handleOnChangeAge = (event) => {
+        this.setState({
+            age: event.target.value
+        });
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                My name is {this.state.name} and I'm {this.state.age};
+                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+                    <label>Your name: </label>
+                    <input
+                        value={this.state.name}
+                        type="text"
+                        onChange={(event) => this.handleOnChangeInput(event)}
+                    />
+                    <button>Submit</button>
+
+                    <label>Your age: </label>
+                    <input
+                        value={this.state.age}
+                        type="text"
+                        onChange={(event) => this.handleOnChangeAge(event)}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UserInfo;
