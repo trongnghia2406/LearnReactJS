36e7909ca01aaf6842a4fa0cd5ea41e514279317
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2025-12-13 09:11:02 +0700

    Chapter 2 - ReactBasic: lesson8: props

--- Chapter2/src/components/MyComponent.js
+++ Chapter2/src/components/MyComponent.js
@@ -1,11 +1,18 @@
 import React from "react";
 import UserInfo from "./UserInfo";
+import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
     // JSX
     render() {
+        const myInfo = ['student', 'developer']
         return (
             <div>
                 <UserInfo />
+                <br />
+                <br />
+                <DisplayInfo name='BangThan' age='21' />
+                <hr />
+                <DisplayInfo name='LeTrongNghia' age={21} myInfo={myInfo} />
             </div>
         );
     }
